37e7b3f5a6b13b470edb86088c9cae12b34cb540
chaerishme <sbk93@naver.com>
2026-06-26 12:56:24 +0900

    feat: todo 개수 계산 useMemo 이용하여 구현

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -1,6 +1,6 @@
 import "./List.css";
 import TodoItem from "./TodoItem";
-import { useState } from "react";
+import { useState, useMemo } from "react";
 
 const List = ({ todos, onUpdate, onDelete }) => {
   const [search, setSearch] = useState("");
@@ -18,9 +18,30 @@ const List = ({ todos, onUpdate, onDelete }) => {
 
   const filteredTodos = getFilteredData();
 
+  const { totalCount, doneCount, notDoneCount } = useMemo(() => {
+    console.log("getAnalyzedData 호출!");
+    const totalCount = todos.length;
+    const doneCount = todos.filter((todo) => todo.isDone).length;
+    const notDoneCount = totalCount - doneCount;
+
+    return {
+      totalCount,
+      doneCount,
+      notDoneCount,
+    };
+  }, [todos]);
+  // 의존성 배열: deps
+  // 의존성 배열에 왜 todos를 넣는지 궁금했는데, totalCount 등은 useMemo의 출력값이라서 순환참조가 되어버림
+  // 따라서 todos가 바뀌는 순간 콜백 재실행 -> count 값 계산하는 것이 맞는 흐름이다. 흐름을 제대로 파악하지 못해서 착각하였다.
+
   return (
     <div className="List">
       <h4>Todo List🌱</h4>
+      <div>
+        <div>total: {totalCount}</div>
+        <div>done: {doneCount}</div>
+        <div>notDone: {notDoneCount}</div>
+      </div>
       <input
         value={search}
         onChange={onChangeSearch}
